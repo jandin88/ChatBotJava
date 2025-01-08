0ced2c2fc3ac5cf552585d525aeee4cec974218b
janderson <jandersson.coutinho@gmail.com>
2025-01-08 11:01:26 -0300

    commit initial

---
+++ src/test/java/org/chat/chatbotjava/ChatBotJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chat.chatbotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
